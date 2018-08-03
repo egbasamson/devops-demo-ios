270ae6b2370da649c64f504aacbf87a64287ef55
Egba Samson <samson.egba@pglnigeria.com>
2018-08-03 15:51:48 +0100

    Second commit

---
+++ DevOpsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevOpsDemo
+//
+//  Created by PYRICH GROUP LIMITED on 03/08/2018.
+//  Copyright © 2018 PYRICH GROUP LIMITED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
