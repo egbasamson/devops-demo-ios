78cf516f471112cbc0cc7fa02909c3df95192caa
Egba Samson <samson.egba@pglnigeria.com>
2018-08-06 09:18:28 +0100

    test failed on xcode.. testing on jenkins now

---
+++ DevOpsDemoTests/ViewControllerTests.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTests.swift
+//  DevOpsDemoTests
+//
+//  Created by PYRICH GROUP LIMITED on 06/08/2018.
+//  Copyright © 2018 PYRICH GROUP LIMITED. All rights reserved.
+//
+
+import XCTest
+@testable import DevOpsDemo
+
+class ViewControllerTests: XCTestCase {
+
+    var viewController: ViewController!
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testIsNumberEven() {
+        viewController = ViewController()
+        let odd = 9
+        XCTAssertTrue(viewController.isNumberEven(num: odd))
+    }
+
+}
